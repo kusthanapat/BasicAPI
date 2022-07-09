71c78d226bd43bd4001615db190d4c1bd5a65011
kusthanapat <102140251+kusthanapat@users.noreply.github.com>
2022-07-10 02:29:47 +0700

    upload function for load  API data from github

--- layout/lib/pages/home.dart
+++ layout/lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:layout/pages/detail.dart';
+import 'package:http/http.dart' as http; // as เป็นการตั้งชื่อเล่น เวาลาเราใช้เรียกตัวนี้ก็ใช่ชื่อ http ทีี่ตั้งไว้หลัง as ได้เลย
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   //const HomePage({ Key? key }) : super(key: key);
@@ -18,18 +20,19 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: FutureBuilder(builder: (context , snapshot) {  
-          var data = json.decode(snapshot.data.toString());   //snapshot เหมือนเป็นข้อมูลทั้งก้อนที่ส่งมาจาก FutureBuilder คือตัว [{},{},{}]
+        child: FutureBuilder(builder: (context , AsyncSnapshot snapshot) {  
+          //var data = json.decode(snapshot.data.toString());   //snapshot เหมือนเป็นข้อมูลทั้งก้อนที่ส่งมาจาก FutureBuilder คือตัว [{},{},{}]
           return ListView.builder(                     //ListView เหมือนการ repeat ข้อมูลออกมาเป็นชุดๆ
             itemBuilder: (BuildContext context , int index) {
-                return mybox(data[index]['title'], data[index]['subtitle'], data[index]['URL_image'],data[index]['detail']);
+                return mybox(snapshot.data[index]['title'], snapshot.data[index]['subtitle'], snapshot.data[index]['URL_image'],snapshot.data[index]['detail']);
             },
-            itemCount: data.length ,
+            itemCount: snapshot.data.length ,
             );
 
 
         },
-        future: DefaultAssetBundle.of(context).loadString('assets/data.json'),//เป็นฟังก์ชั่นพิเศษเหมือนการดึงไฟล์data.jsonมาแล้วโยนไปให้ snapshot
+        future: getData(),
+        //future: DefaultAssetBundle.of(context).loadString('assets/data.json'),//เป็นฟังก์ชั่นพิเศษเหมือนการดึงไฟล์data.jsonมาแล้วโยนไปให้ snapshot
                                                                               
         )  //future เหมือนเป็นการรันคำสั่งหลายอันพร้อมกัน ใครเสร็จก่อนก็ไปแสดงผลก่อน
       ),
@@ -57,7 +60,7 @@ class _HomePageState extends State<HomePage> {
           colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.615), BlendMode.darken)
         )
       ),
-      padding: EdgeInsets.all(15),
+      padding: EdgeInsets.all(10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,17 +69,29 @@ class _HomePageState extends State<HomePage> {
           SizedBox(height: 15,),
           Text(subtitle,style: TextStyle(fontSize: 20,color: Colors.white),
           ),
-          SizedBox(height: 18,),
+          SizedBox(height: 15,),
           TextButton(onPressed: () {
             print('Next Page');
             Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(v1,v2,v3,v4)));
-          }, child: Text('Continue'))
+          }, child: Text('Continue...',style: TextStyle(color: Colors.white70),))
         
         ],
       ),
     );
   }
+
+
+  Future getData() async {
+    //https://raw.githubusercontent.com/kusthanapat/BasicAPI/main/data.json
+    var URL = Uri.https('raw.githubusercontent.com', '/kusthanapat/BasicAPI/main/data.json');
+    var response = await http.get(URL);         // await เป็นฟังก์ชั่นที่ใช้คู่กับ async
+    var result = json.decode(response.body);
+    return result;
+  }
+
+
 }
+
 
 
 
